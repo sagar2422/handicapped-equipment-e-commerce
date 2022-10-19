278e18168ee73f1fc283d1f20bff0586685690ed
sanket-16 <san162002@gmail.com>
2022-10-19 12:21:24 +0530

    revert

--- server/app.js
+++ server/app.js
@@ -4,10 +4,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const dbConnection = require('./db') // loads our connection to the mongo database
-const passport = require('./passport');
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
 
@@ -18,19 +14,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(
-	session({
-		secret: process.env.APP_SECRET ,
-		store: new MongoStore({ mongooseConnection: dbConnection }),
-		resave: false,
-		saveUninitialized: false
-	})
-)
-
-// ===== Passport ====
-app.use(passport.initialize())
-app.use(passport.session()) // will call the deserializeUser
-
+mongoose.connect(process.env.MONGO_DB, ()=> {
+    console.log('connected to db!')
+});
 
 //ROUTES
 app.use('/api/user',authRoutes);
@@ -40,4 +26,5 @@ const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+}
+)
