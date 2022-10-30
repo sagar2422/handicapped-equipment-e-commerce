0af96b4795a94b2e13b50c8306e022f1ad21e20f
sanket-16 <san162002@gmail.com>
2022-10-30 12:11:29 +0530

    final changes and deleting temp database

--- server/controllers/campaign/addCampaign.js
+++ server/controllers/campaign/addCampaign.js
@@ -1,24 +1,25 @@
 const { Campaign } = require('../../models/Campaign');
 
 async function addCampaign(req, res) {
-	const { id, productId, title, description,total } = req.body;
-    console.log(req.body)
+	const { id, productId, title, description, total } = req.body;
+	console.log(req.body);
 	try {
 		const campaign = new Campaign({
 			userId: id,
-            completedAmount:0,
+			completedAmount: 0,
 			productId,
-            total,
+			total,
 			title,
 			description,
 		});
 		campaign.save();
-        console.log(campaign,'here')
+		console.log(campaign, 'here');
 		res.status(200).json({ campaign });
 	} catch (error) {
 		console.log(error);
-        res.status(404).json(error);
+		res.status(404).json(error);
 	}
 }
 
 module.exports = addCampaign;
+
